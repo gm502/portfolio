2182550b6e46bce78f9ed993f125fd4b2a1b1912
Gabo <mishaan.gabo@gmail.com>
2022-10-07 21:42:20 -0400

    href updates

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -73,7 +73,7 @@ function Home2() {
               </li>  
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/gabo-m-3a4119244/"
+                  href="www.linkedin.com/in/gabo-mishaan"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
